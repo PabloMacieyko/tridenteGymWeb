97876c43a04bd3fdb180fa66b02bcf9a4d327438
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-06-27 01:04:43 -0300

    chau icon

--- src/components/activityItem/ActivityItem.jsx
+++ src/components/activityItem/ActivityItem.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ActivityItem = ({ name, price, description, icon }) => {
+const ActivityItem = ({ name, price, description }) => {
 
   return (
     <div className="card text-center space-y-3 sm:space-y-6 p-4 sm:py-16 bg-gray-200 dark:bg-dark hover:bg-primary/20 dark:hover:bg-primary/50 duration-300 text-black dark:text-white rounded-lg group">
-      <div className="item-center justify-center flex">
-        {icon}
-      </div>
       <h1 className="text-3xl font-bold">{name}</h1>
       <h1 className="text-center text-4xl font-semibold text-primary">
         ${price}
